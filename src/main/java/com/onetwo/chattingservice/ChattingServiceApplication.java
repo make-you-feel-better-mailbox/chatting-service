e984a8c69bebc0bdf7f3a595011ec43a24f4c087
Jeong Jeongil <jji042842@gmail.com>
2024-05-16 13:48:19 +0900

    update db and config

--- src/main/java/com/onetwo/chattingservice/ChattingServiceApplication.java
+++ src/main/java/com/onetwo/chattingservice/ChattingServiceApplication.java
@@ -1,5 +1,6 @@
 package com.onetwo.chattingservice;
 
+import com.onetwo.chattingservice.common.GlobalStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChattingServiceApplication {
 
     public static void main(String[] args) {
+        String active = System.getProperty(GlobalStatus.SPRING_PROFILES_ACTIVE);
+        if (active == null) {
+            System.setProperty(GlobalStatus.SPRING_PROFILES_ACTIVE, GlobalStatus.SPRING_PROFILES_ACTIVE_DEFAULT);
+        }
+
+        System.setProperty(
+                GlobalStatus.SPRING_PROFILES_ACTIVE,
+                System.getProperty(GlobalStatus.SPRING_PROFILES_ACTIVE, GlobalStatus.SPRING_PROFILES_ACTIVE_DEFAULT)
+        );
         SpringApplication.run(ChattingServiceApplication.class, args);
     }
 
---
+++ src/main/java/com/onetwo/chattingservice/common/GlobalStatus.java
@@ -0,0 +1,8 @@
+package com.onetwo.chattingservice.common;
+
+public class GlobalStatus {
+
+    public static final Boolean PERSISTENCE_NOT_DELETED = false;
+    public static final String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+    public static final String SPRING_PROFILES_ACTIVE_DEFAULT = "local";
+}
